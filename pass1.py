8dd75939dadee2eb8521af0ae2c0e995df0a843a
Srinivas31709 <2000031709@kluniversity.in>
2022-01-08 08:50:42 +0530

    output available

--- pass1.py
+++ pass1.py
@@ -1,7 +1,11 @@
 #this change has been made now
-
+#using pass and format function
 for numb in range(1,5):
     if numb==2:
         pass
     else:
         print ("Present Number =  {} ".format(numb))
+#Output is:
+#Present Number =  1
+#Present Number =  3
+#Present Number =  4
